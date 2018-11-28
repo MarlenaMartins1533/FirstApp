10e2d8b15fec8eb622d0a03f4ef5b93982742dca
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2018-11-28 17:29:19 -0300

    Inserindo dot, reparando resultado, realizando cálculo de decimais, tratando zeros a esquerda.

--- app/src/main/java/com/marlena/firstapp/MainActivity.kt
+++ app/src/main/java/com/marlena/firstapp/MainActivity.kt
@@ -8,18 +8,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var text = "0"
-    private var num1: Int? = null
-    private var num2: Int? = null
+    private var num1: String? = null
+    private var num2: String? = null
     private var signal: Char? = null
     private var result = false
-    private var comma = false
-    private var commanext = false
+    private var dot = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        igualTXT.setOnClickListener {
+        equalTXT.setOnClickListener {
             if (signal != null && num1 != null && num2 != null){
                 when (signal) {
                     '+' -> num1 = sum(num1!!, num2!!)
@@ -27,38 +26,38 @@ class MainActivity : AppCompatActivity() {
                     'x' -> num1 = mul(num1!!, num2!!)
                     '/' -> num1 = div(num1!!, num2!!)
                 }
-                displayTXT.text = num1.toString()
+                displayTXT.text = num1
                 result = true
                 num2 = null
                 signal = null
-                comma = false
-                commanext = false
+                dot = false
+            }
+            if (signal != null && num1 != null && num2 == "0") {
+
             }
         }
 
         zeroTXT.setOnClickListener {
-            if ((num1 == null) || (num1 == 0) || result ) {
+            if ((num1 == null) || result ) {
                 result = false
-
+                num1 = null //anula num1 se for resultado
                 text = "0"
-                num1 = 0
                 displayTXT.text = text
             }
             else if (signal == null) {
-                text = num1.toString() + "0"
+                text = num1 + "0"
                 displayTXT.text = text
-                num1 = text.toInt()
+                num1 = text
             }
-            else if((num2 == null) || (num2 == 0)) {
-                text = num2.toString() + "0"
+            else if(num2 == null) {
+                text = "$num1$signal$text"
                 displayTXT.text = text
-                num2 = text.toInt()
             }
             else {
-
-                text = num2.toString() + "0"
-                displayTXT.text = "$num1$signal$text"
-                num2 = text.toInt()
+                text = num2 + "0"
+                num2 = text
+                text = "$num1$signal$text"
+                displayTXT.text = text
             }
         }
         oneTXT.setOnClickListener {
@@ -82,11 +81,12 @@ class MainActivity : AppCompatActivity() {
 
         plusTXT.setOnClickListener {
             result = false
-            /*if (num1 == null) num1 = 0
-            * if commanext == true*/
+            if (num1 == null) num1 = "0"
 
-            if ((num2 == null) && (signal != null)) signal = '+'
-            else {
+            if ((num2 == null) && (signal != null)) {
+                signal = '+'
+
+            } else {
                 if (num2 != null && signal != null){
                     when (signal) { //nos outros sinais não precisa verificar prioridade aritmetica ainda
                         '+' -> num1 = sum(num1!!, num2!!)
@@ -94,128 +94,194 @@ class MainActivity : AppCompatActivity() {
                         'x' -> num1 = mul(num1!!, num2!!)
                         '/' -> num1 = div(num1!!, num2!!)
                     }
-                    displayTXT.text = num1.toString()
+                    displayTXT.text = num1
                     num2 = null
-                    comma = false
-                    commanext = false
                     signal = '+'
                 }
             }
             signal = '+'
+            dot = false
             text = "$num1$signal"
             displayTXT.text = text
         }
 
         minusTXT.setOnClickListener {
             result = false
+            if (num1 == null) num1 = "0"
 
-            if ((num2 == null) || (signal != null)){
-                signal = '-'
-                text = "$num1$signal"
-                displayTXT.text = text
-            }
-            else {
-                num1 = sub(num1!!, num2!!)
-                num2 = null
+            if ((num2 == null) && (signal != null)) {
                 signal = '-'
 
-                text = "$num1$signal"
-                displayTXT.text = text
+            } else {
+                if (num2 != null && signal != null){
+                    when (signal) { //nos outros sinais não precisa verificar prioridade aritmetica ainda
+                        '+' -> num1 = sum(num1!!, num2!!)
+                        '-' -> num1 = sub(num1!!, num2!!)
+                        'x' -> num1 = mul(num1!!, num2!!)
+                        '/' -> num1 = div(num1!!, num2!!)
+                    }
+                    displayTXT.text = num1
+                    num2 = null
+                    signal = '-'
+                }
             }
+            signal = '-'
+            dot = false
+            text = "$num1$signal"
+            displayTXT.text = text
         }
 
         timesTXT.setOnClickListener {
             result = false
+            if (num1 == null) num1 = "0"
 
-            if ((num2 == null) || (signal != null)){
-                signal = 'x'
-                text = "$num1$signal"
-                displayTXT.text = text
-            }
-            else {
-                num1 = mul(num1!!, num2!!)
-                num2 = null
+            if ((num2 == null) && (signal != null)) {
                 signal = 'x'
 
-                text = "$num1$signal"
-                displayTXT.text = text
+            } else {
+                if (num2 != null && signal != null){
+                    when (signal) { //nos outros sinais não precisa verificar prioridade aritmetica ainda
+                        '+' -> num1 = sum(num1!!, num2!!)
+                        '-' -> num1 = sub(num1!!, num2!!)
+                        'x' -> num1 = mul(num1!!, num2!!)
+                        '/' -> num1 = div(num1!!, num2!!)
+                    }
+                    displayTXT.text = num1
+                    num2 = null
+                    signal = 'x'
+                }
             }
+            signal = 'x'
+            dot = false
+            text = "$num1$signal"
+            displayTXT.text = text
         }
 
         barTXT.setOnClickListener {
             result = false
+            if (num1 == null) num1 = "0"
 
-            if ((num2 == null) || (signal != null)){
-                signal = '/'
-                text = "$num1$signal"
-                displayTXT.text = text
-            }
-            else {
-                num1 = div(num1!!, num2!!)
-                num2 = null
+            if ((num2 == null) && (signal != null)) {
                 signal = '/'
 
-                text = "$num1$signal"
-                displayTXT.text = text
+            } else {
+                if (num2 != null && signal != null){
+                    when (signal) { //nos outros sinais não precisa verificar prioridade aritmetica ainda
+                        '+' -> num1 = sum(num1!!, num2!!)
+                        '-' -> num1 = sub(num1!!, num2!!)
+                        'x' -> num1 = mul(num1!!, num2!!)
+                        '/' -> num1 = div(num1!!, num2!!)
+                    }
+                    displayTXT.text = num1
+                    num2 = null
+                    signal = '/'
+                }
             }
+            signal = '/'
+            dot = false
+            text = "$num1$signal"
+            displayTXT.text = text
         }
 
-        commaTXT.setOnClickListener {
-            if (comma == true) {
-                text = displayTXT.text.toString()
-                displayTXT.text = text
+        dotTXT.setOnClickListener {
+            when {
+                num1 == null -> {
+                    dot = true
+                    num1 = "0."
+                    text = "0."
+                    displayTXT.text = text
+                }
+                num1 != null && signal == null && !dot -> {
+                    dot = true
+                    num1 = "$num1."
+                    displayTXT.text = num1
+                }
+                //se num1 != null && signal == null && dot = true passa direto sem fazer nada
+
+                num2 == null && signal != null && !dot -> {
+                    dot = true
+                    num2 = "0."
+                    text = "$num1$signal$num2"
+                    displayTXT.text = text
+                    //num1 = text.toDouble()
             }
-            else {
-                comma = true
-                commanext = true
-                text = displayTXT.text.toString() + ','
-                displayTXT.text = text
+                num2 != null && signal != null && !dot -> {
+                    dot = true
+                    num2 = "$num2."
+                    text = "$num1$signal$num2"
+                    displayTXT.text = text
+                }
+
             }
         }
     }
 
-    private fun sum(num1: Int, num2:Int): Int {
+    private fun sum(num1: String, num2:String): String? {
         signal = null
-        return num1 + num2
+        val num: Double?
+        val xnum1: Double = num1.toDouble()
+        val xnum2: Double = num2.toDouble()
+
+        num = xnum1 + xnum2
+        text = num.toString()
+        return text
 
     }
-    private fun sub(num1: Int, num2:Int): Int{
+    private fun sub(num1: String, num2:String): String?{
         signal = null
-        return num1 - num2
+        val num: Double?
+        val xnum1: Double = num1.toDouble()
+        val xnum2: Double = num2.toDouble()
+
+        num = xnum1 - xnum2
+        text = num.toString()
+        return text
     }
-    private fun mul(num1: Int, num2:Int) : Int{
+    private fun mul(num1: String, num2:String) : String?{
         signal = null
-        return num1 * num2
+        val num: Double?
+        val xnum1: Double = num1.toDouble()
+        val xnum2: Double = num2.toDouble()
+
+        num = xnum1 * xnum2
+        text = num.toString()
+        return text
     }
-    private fun div(num1: Int, num2:Int) : Int{
+    private fun div(num1: String, num2:String) : String{
         signal = null
-        return num1 / num2
+        val num: Double?
+        val xnum1: Double = num1.toDouble()
+        val xnum2: Double = num2.toDouble()
+
+        num = xnum1 / xnum2
+        text = num.toString()
+        return text
     }
 
     private fun clickNum(numTXT: TextView){
 
-        val num = numTXT.text.toString().toInt()
+        val num: String? = numTXT.text.toString()
         text = displayTXT.text.toString()
 
-        if (num1 == null || num1 == 0 || result) {
+        if ((num1 == null) || result) {
             result = false
-
             num1 = num
             displayTXT.text = numTXT.text
         }
         else if (signal == null) {
-            text = "$num1$num"
-            displayTXT.text = text
-            num1 = text.toInt()
+                text = "$num1$num"
+                displayTXT.text = text
+                num1 = text
         }
-        else if (num2 == null || num2 == 0) {
-            num2 = num
-            text = "$num1$signal$num2"
-            displayTXT.text = text
-        } else {
+        else if (num2 == null) {
+                num2 = num
+                text = "$num1$signal$num2"
+                displayTXT.text = text
+        }
+        else {
             //adicionando numero ao num2
             text = "$num2$num"
-            num2 = text.toInt()
+            num2 = text
 
             //colocando tudo no display
             text = "$num1$signal$num2"
